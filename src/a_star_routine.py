530651f4d59a6b2cb6b2b7d70ef4fcbd7e59a594
Julia Chun <hjchun96@gmail.com>
2019-08-04 19:39:58 -0400

    merge wip

--- src/a_star_routine.py
+++ src/a_star_routine.py
@@ -3,6 +3,9 @@ import time
 from graph import Graph
 from collections import defaultdict
 
+# Custom Imports
+from Warrior import Warrior
+
 # -- Initialize game
 # Footbal field: 8 x 5
 #
@@ -28,6 +31,9 @@ from collections import defaultdict
 # (0,0) ☐══☐══☐══☐══☐══☐
 # --- start ---
 
+game_over = False
+
+## Graph Construction
 obstacles = defaultdict(bool)
 obstacle_edges = [
     ((0,0), (0,1)), ((1,0), (1,1)),
@@ -38,9 +44,17 @@ obstacle_edges = [
 for edges in obstacle_edges:
     obstacles[edges] = True
 
+## Set Up Agents
 
-good_agent = "D2-0709"
+good_agent = ["D2-0709"]
 bad_agents = ["D2-4D79", "D2-6F8D"]
+
+good_agent1 = Warrior('Q5-8CC0', (0, 0), True)
+good_agent2 = Warrior('Q5-8CC0', (0, 4), True)
+
+bad_agent1 = Warrior('D2-0709', (6, 0), False)
+bad_agent2 = Warrior('Q5-8CC0', (6, 4), False)
+
 
 G = Graph(
     obstacles = obstacles,
@@ -53,15 +67,16 @@ G = Graph(
 )
 
 goal = (4, 7)
+
 print("Game start!")
 G.print()
-
 while True:
-    if(G.agent_positions[good_agent] == goal):
-        break
+
+    # if(G.agent_positions[good_agent] == goal):
+    #     break
 
     # GOOD AGENT
-    path_good_agent = G.get_agent_move(good_agent,
+    path_good_agent = get_agent_move(good_agent,
         strategy = "A_star",
         min_bound = G.get_agent_position(good_agent),
         max_bound = goal)
@@ -69,16 +84,17 @@ while True:
     G.print()
 
     # BAD AGENTS
-    path_bad_agent_0 = G.get_agent_move(bad_agents[0],
+    path_bad_agent_0 = get_agent_move(bad_agents[0],
         strategy = "horizontal",
         min_bound = (1,6),
         max_bound = (3,6))
     G.move_agent(bad_agents[0], path_bad_agent_0)
     G.print()
 
-    path_bad_agent_1 = G.get_agent_move(bad_agents[1],
+    path_bad_agent_1 = get_agent_move(bad_agents[1],
         strategy = "vertical",
         min_bound = (0,3),
         max_bound = (0,7))
+
     G.move_agent(bad_agents[1], path_bad_agent_1)
     G.print()
---
+++ src/game_helpers.py
@@ -0,0 +1,95 @@
+import random
+import maneuver
+
+def good_droid_turn(droid, w, G, victory_state, warriors):
+
+    if not (droid.get_is_alive()):
+        print("YOU LOST")
+        # droid falls over
+        game_over = True
+
+    path = A_star(G, droid.get_location, victory_state)
+    path = get_path(droid, path)
+
+    x, y = path[0]
+    # Update grid states
+    # Update agent's position
+    G.update_agent_position(droid.ID, droid.get_location())
+    # Update grid so that old position (x,y) is now available for move into
+    # while new position is not available into
+    G.update_neighbors((x,y), droid.get_location())
+
+    maneuver.follow_path(droid, path, speed = 0x88, scale_dist = 1)
+
+    if droid in victory_state:
+        print("YOU WON")
+        # chirping Sound
+        agent_droid.animate(5)
+        # headspin
+    else:
+
+        dist, bad_droid = get_nearest_opponent(droid.get_location(), warriors)
+        if 1 < dist < 2:
+            bad_droid.set_is_active(False)
+            # headspin
+
+    return True
+
+def bad_droid_turn(droid, warriors, G):
+    if not droid.get_is_active():
+        droid.set_is_active(True)
+        return
+
+    dist, closest_droid = get_nearest_opponent(droid.get_location(), warriors)
+    path = a_star(G, droid.get_location(), closest_droid.get_location())
+
+    #TODO: update graph
+
+    if len(path) > 1:
+        if got_speed_boost():
+            path = path[0:3]
+        else:
+            path = path[0:2]
+    # path = get_path(droid, path)
+
+
+def launch_EMP(self, bad_guy):
+    bad_guy.set_is_active(False)
+    use_weapon("EMP")
+
+def got_speed_boost():
+    return random.random() < 0.2
+
+def get_nearest_opponent(location, warriors):
+
+    d_min = math.inf
+    opponent = None
+    for w in warriors:
+        if w.get_is_good():
+            dist = compute_distance(location, w.get_location())
+                if dist < d_min:
+                    d_min = dist
+                    opponent = w
+
+    return d_min, opponent
+
+
+def get_path(droid, path):
+
+     if path is None:
+        return False
+
+    if got_speed_boost():
+        path = path[0:3]
+        droid.set_location = path[2]
+    else:
+        path = path[0:2]
+        droid.set_location = path[1]
+
+    return path
+
+def compute_distance(location1, location2):
+    x_1, y_1 = location1
+    x_2, y_2 = location2
+    d_x, d_y = (x_2 - x_1), (y_2 - y_1)
+    return math.sqrt(d_x**2 + d_y**2)
